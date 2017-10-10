f47bef05f63c7f0fcaea790d4735813cea328de6
Han Gia Pham <hgpham@andromeda-33.ics.uci.edu>
2017-10-10 16:41:13 -0700

    First commit

---
+++ Wumpus_Shell/src/Map.hpp
@@ -0,0 +1,15 @@
+#include <vector> 
+
+// Describes the findings of each node
+class Cell
+{
+	bool visited;
+	float wumpusPresent;
+	float pitPresent;
+}
+
+// Keep track of the path agent has taken
+class Map
+{
+	vector<vector<Cell>> map;
+}
